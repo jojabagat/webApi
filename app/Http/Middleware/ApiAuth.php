8fa55ee731b0efc091a75957195d097772c20e2a
Josephine Jabagat <30436769+jojabagat@users.noreply.github.com>
2017-10-02 21:36:55 +0800

    Add files via upload

---
+++ app/Http/Middleware/ApiAuth.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use App\User;
+
+class ApiAuth
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $user = User::all();
+        foreach($user as $user)
+        {
+            if($request->key == $user->api_token)
+                return $next($request); 
+        }
+        return response()->json(array("Authentication"=>"Wrong key"));
+    }
+}
